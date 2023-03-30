9da327f5869989eb02fed634d7d516f7ee577acb
Scott McKay <skottmckay@gmail.com>
2023-03-30 18:28:57 +1000

    Add ability to use url or local file with update script.

--- tools/ci_build/github/apple/update_swift_package_manager_config.py
+++ tools/ci_build/github/apple/update_swift_package_manager_config.py
@@ -2,6 +2,7 @@ import argparse
 import hashlib
 import os
 import pathlib
+from typing import Union
 
 
 def calc_checksum(filename: pathlib.Path):
@@ -13,36 +14,63 @@ def calc_checksum(filename: pathlib.Path):
     return sha256_hash.hexdigest()
 
 
-# find this section in Package.swift and replace the values for url and checksum
-# 'url:' has to be converted to 'path:' to work with a filepath
+def checksum_remote_package(package_url: str):
+    from urllib.request import urlretrieve
+    from urllib.parse import urlparse
+    import tempfile
+
+    # tempfile.NamedTemporaryFile returns an open file, but we need a filename
+    with tempfile.TemporaryDirectory() as tmp:
+        filename = os.path.basename(package_url)
+        assert filename
+
+        temp_file = os.path.join(tmp, filename)
+        print(f"Downloading file from {package_url} to {temp_file}")
+        urlretrieve(package_url, filename=temp_file)
+        return calc_checksum(temp_file)
+
+
+# find this section in Package.swift and replace the values for url and checksum.
+# If replacing with a local file, 'url:' -> 'file:' and we don't have a checksum
 #
 #   .binaryTarget(name: "onnxruntime",
 #      url: "https://onnxruntimepackages.z14.web.core.windows.net/pod-archive-onnxruntime-c-1.14.0.zip",
 #      checksum: "c89cd106ff02eb3892243acd7c4f2bd8e68c2c94f2751b5e35f98722e10c042b"),
 #
+def update_swift_package(spm_config_path: pathlib.Path, ort_package_path: Union[pathlib.Path, str]):
+    # ort_package_path must be a str for a url or Path for local file
+    is_url = type(ort_package_path) is str
+    if is_url:
+        checksum = checksum_remote_package(ort_package_path)
+    else:
+        checksum = calc_checksum(ort_package_path)
 
-def update_swift_package(spm_config_path: pathlib.Path, ort_package_path: pathlib.Path):
     updated = False
     new_config = []
-    checksum = calc_checksum(ort_package_path)
-
     with open(spm_config_path, "r") as config:
         while line := config.readline():
             if '.binaryTarget(name: "onnxruntime"' in line:
-                # get and update the following 2 lines with url and checksum
+                # get and update the following 2 lines
                 url_line = config.readline()
                 checksum_line = config.readline()
                 assert "url:" in url_line
                 assert "checksum:" in checksum_line
 
-                start_url = url_line.find('url:')
-                start_checksum_value = checksum_line.find('"')
-                new_url_line = url_line[: start_url] + f'file: "{ort_package_path}",\n'
-                new_checksum_line = checksum_line[: start_checksum_value + 1] + checksum + '"),\n'
+                if is_url:
+                    start_url_value = url_line.find('"')
+                    new_url_line = url_line[: start_url_value + 1] + str(ort_package_path) + '",\n'
+                else:
+                    start_url = url_line.find('url:')
+                    new_url_line = url_line[: start_url] + f'file: "{ort_package_path}")\n'
 
                 new_config.append(line)
                 new_config.append(new_url_line)
-                new_config.append(new_checksum_line)
+
+                if is_url:
+                    start_checksum_value = checksum_line.find('"')
+                    new_checksum_line = checksum_line[: start_checksum_value + 1] + checksum + '"),\n'
+                    new_config.append(new_checksum_line)
+
                 updated = True
             else:
                 new_config.append(line)
@@ -70,13 +98,18 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--ort_package",
-        type=pathlib.Path,
+        type=str,
         required=True,
-        help="ORT native iOS pod to use. Filename should be something like pod-archive-onnxruntime-c-x.y.z.zip",
+        help="ORT native iOS pod to use. Can be a url starting with 'http' or local file. "
+             "The package name should be something like pod-archive-onnxruntime-c-<version info>.zip.",
     )
 
     args = parser.parse_args()
     spm_config = args.spm_config.resolve(strict=True)
-    ort_package = args.ort_package.resolve(strict=True)
+
+    if args.ort_package.startswith("http"):
+        ort_package = args.ort_package
+    else:
+        ort_package = pathlib.Path(args.ort_package).resolve(strict=True)
 
     update_swift_package(spm_config, ort_package)
