733bc84a32b6629f67eb0f3fbfb76576ec406b15
yf711 <yifanl@microsoft.com>
2023-05-12 15:12:35 -0700

    update win trt folder

--- onnxruntime/python/tools/tensorrt/trt_update/trt_version_updater.py
+++ onnxruntime/python/tools/tensorrt/trt_update/trt_version_updater.py
@@ -1,5 +1,20 @@
 ﻿import os
 
+# Global config
+dir_path = "../../../../../tools/ci_build/github/azure-pipelines/"
+output_file = "replacement_log.md"
+repo_url = "https://github.com/microsoft/onnxruntime/tree/main/"
+prompts = {
+    "general": {
+        "keywords_input": "Enter keywords separated by comma (press Enter to skip):\n\t",
+        "replacements_input": "Enter replacement words separated by comma:\n\t"
+        },
+    "win_trt": {
+        "old_win_trt_folder": "Enter folder path to old trt binaries. e.g TensorRT-8.5.3.1.Windows10.x86_64.cuda-11.8.cudnn8.6\n\t",
+        "new_win_trt_folder": "Enter folder path to new win trt binaries which has been uploaded to Azure blob storage (press Enter to skip)\n\t e.g TensorRT-8.6.1.6.Windows10.x86_64.cuda-11.8\TensorRT-8.6.1.6, \n\t\twhich is downloadable via https://developer.nvidia.com/nvidia-tensorrt-download\n\t"
+        }
+    }
+
 # Recursively traverse all files in a directory
 def list_files(dir_path):
     file_list = []
@@ -24,30 +39,35 @@ def search_and_replace_keywords(file_path, keywords, replacements):
             file.write(line)
     return result
 
-def exec(output_file):
-    dir_path = "../../../../../tools/ci_build/github/azure-pipelines/"
-    keywords_input = input("Please enter keywords separated by comma: ")
-    replacements_input = input("Please enter replacement words separated by comma: ")
+def updater():
+    # update general keyword
+    keywords_input = input(prompts["general"]["keywords_input"])
+    replacements_input = input(prompts["general"]["replacements_input"])
     keywords = [keyword.strip() for keyword in keywords_input.split(",")]
     replacements = [replacement.strip() for replacement in replacements_input.split(",")]
-    file_list = list_files(dir_path)
+    assert len(replacements) == len(keywords), "keywords and replacements should have same amount"
     
-    repo_url = "https://github.com/microsoft/onnxruntime/tree/main/"
+    # update win trt folder
+    old_win_trt_folder = input(prompts["win_trt"]["old_win_trt_folder"])
+    new_win_trt_folder = input(prompts["win_trt"]["new_win_trt_folder"])
+    assert new_win_trt_folder.startswith("TensorRT-"), "Please doublecheck folder name of TRT binaries folder"
+    keywords.insert(0, old_win_trt_folder)
+    replacements.insert(0, new_win_trt_folder)
 
+    file_list = list_files(dir_path)
     with open(output_file, "w", encoding="utf-8") as md_file:
         for file_path in file_list:
+            clean_file_path = file_path.split("../")[-1].replace("\\", "/")
             replaced_result = search_and_replace_keywords(file_path, keywords, replacements)
             if replaced_result:
-                md_file.write(f"## {os.path.relpath(file_path, dir_path)}\n")
+                md_file.write(f"## {clean_file_path}\n")
                 for line_num, line in replaced_result:
-                    relative_file_path = os.path.relpath(file_path, dir_path)
-                    file_url = f"{repo_url}{relative_file_path}#L{line_num}"
+                    file_url = f"{repo_url}{clean_file_path}#L{line_num}"
                     md_file.write(f"- Line {line_num} (replaced): [`{line}`]({file_url})\n")
                 md_file.write("\n")
 
 def main():
-    output_file = "log.md"
-    exec(output_file)
+    updater()
 
 if __name__ == "__main__":
     main()
