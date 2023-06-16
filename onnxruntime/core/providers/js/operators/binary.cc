0c83f772391a9297e7a8a8e5b45461e34370ecaa
guschmue <guschmue@microsoft.com>
2023-06-16 13:02:38 -0700

    works

--- onnxruntime/core/providers/js/operators/binary.cc
+++ onnxruntime/core/providers/js/operators/binary.cc
@@ -12,7 +12,7 @@ namespace js {
       kOnnxDomain,                                                                 \
       VERSION,                                                                     \
       kJsExecutionProvider,                                                        \
-      KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<TYPE>()), \
+      KernelDefBuilder().TypeConstraint("T", TYPE()),                              \
       KERNEL_CLASS);
 
 #define REG_ELEMENTWISE_VERSIONED_KERNEL(OP_TYPE, VERSION_FROM, VERSION_TO, TYPE, KERNEL_CLASS) \
@@ -21,34 +21,34 @@ namespace js {
       kOnnxDomain,                                                                              \
       VERSION_FROM, VERSION_TO,                                                                 \
       kJsExecutionProvider,                                                                     \
-      KernelDefBuilder().TypeConstraint("T", DataTypeImpl::GetTensorType<TYPE>()),              \
+      KernelDefBuilder().TypeConstraint("T", TYPE()),                                           \
       KERNEL_CLASS);
 
 JSEP_KERNEL_IMPL(Add, Add)
-REG_ELEMENTWISE_VERSIONED_KERNEL(Add, 7, 12, float, Add);
-REG_ELEMENTWISE_VERSIONED_KERNEL(Add, 13, 13, float, Add);
-REG_ELEMENTWISE_KERNEL(Add, 14, float, Add);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Add, 7, 12, DataTypeImpl::AllFixedSizeTensorTypes, Add);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Add, 13, 13, DataTypeImpl::AllFixedSizeTensorTypes, Add);
+REG_ELEMENTWISE_KERNEL(Add, 14, DataTypeImpl::AllFixedSizeTensorTypes, Add);
 
 JSEP_KERNEL_IMPL(Sub, Sub)
-REG_ELEMENTWISE_VERSIONED_KERNEL(Sub, 7, 12, float, Sub);
-REG_ELEMENTWISE_VERSIONED_KERNEL(Sub, 13, 13, float, Sub);
-REG_ELEMENTWISE_KERNEL(Sub, 14, float, Sub);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Sub, 7, 12, DataTypeImpl::GetTensorType<float>, Sub);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Sub, 13, 13, DataTypeImpl::GetTensorType<float>, Sub);
+REG_ELEMENTWISE_KERNEL(Sub, 14, DataTypeImpl::GetTensorType<float>, Sub);
 
 JSEP_KERNEL_IMPL(Mul, Mul)
-REG_ELEMENTWISE_VERSIONED_KERNEL(Mul, 7, 12, float, Mul);
-REG_ELEMENTWISE_VERSIONED_KERNEL(Mul, 13, 13, float, Mul);
-REG_ELEMENTWISE_KERNEL(Mul, 14, float, Mul);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Mul, 7, 12, DataTypeImpl::GetTensorType<float>, Mul);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Mul, 13, 13, DataTypeImpl::GetTensorType<float>, Mul);
+REG_ELEMENTWISE_KERNEL(Mul, 14, DataTypeImpl::GetTensorType<float>, Mul);
 
 JSEP_KERNEL_IMPL(Div, Div)
-REG_ELEMENTWISE_VERSIONED_KERNEL(Div, 7, 12, float, Div);
-REG_ELEMENTWISE_VERSIONED_KERNEL(Div, 13, 13, float, Div);
-REG_ELEMENTWISE_KERNEL(Div, 14, float, Div);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Div, 7, 12, DataTypeImpl::GetTensorType<float>, Div);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Div, 13, 13, DataTypeImpl::GetTensorType<float>, Div);
+REG_ELEMENTWISE_KERNEL(Div, 14, DataTypeImpl::GetTensorType<float>, Div);
 
 JSEP_KERNEL_IMPL(Pow, Pow)
-REG_ELEMENTWISE_VERSIONED_KERNEL(Pow, 7, 11, float, Pow);
-REG_ELEMENTWISE_VERSIONED_KERNEL(Pow, 12, 12, float, Pow);
-REG_ELEMENTWISE_VERSIONED_KERNEL(Pow, 13, 14, float, Pow);
-REG_ELEMENTWISE_KERNEL(Pow, 15, float, Pow);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Pow, 7, 11, DataTypeImpl::GetTensorType<float>, Pow);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Pow, 12, 12, DataTypeImpl::GetTensorType<float>, Pow);
+REG_ELEMENTWISE_VERSIONED_KERNEL(Pow, 13, 14, DataTypeImpl::GetTensorType<float>, Pow);
+REG_ELEMENTWISE_KERNEL(Pow, 15, DataTypeImpl::GetTensorType<float>, Pow);
 
 }  // namespace js
 }  // namespace onnxruntime
