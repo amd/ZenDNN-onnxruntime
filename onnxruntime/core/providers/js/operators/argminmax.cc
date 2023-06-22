077a42c9031cf6c9427513318409a184a53fca04
guschmue <guschmue@microsoft.com>
2023-06-22 13:10:43 -0700

    webgpu: argminmax op defs

--- onnxruntime/core/providers/js/operators/argminmax.cc
+++ onnxruntime/core/providers/js/operators/argminmax.cc
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include "argmax.h"
+#include "argminmax.h"
 
 namespace onnxruntime {
 namespace js {
